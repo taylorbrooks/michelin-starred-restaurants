e354d8d5005c08b0bf68c5f37392e235f809627b
Taylor Brooks <tbrooks@gmail.com>
2019-01-22 09:47:30 -0600

    Use correct spacing Link to artoo source Comment of option to save as JSON

--- scraper.js
+++ scraper.js
@@ -1,3 +1,4 @@
+// Set artoo as a bookmarklet http://medialab.github.io/artoo
 // Go here: https://www.viamichelin.com/web/Restaurants/Restaurants-United_States
 // Load artoo.js
 // Copy and run the following script in the web console
@@ -5,33 +6,33 @@
 var listScraper = {
   iterator: 'li.poi-item-restaurant',
   data: {
-  	name: {sel: '.poi-item-name'},
-  	address: {sel: '.poi-item-details', attr: 'title'},
-  	link: {sel: '.poi-item-name a', attr: 'href'},
+    name: {sel: '.poi-item-name'},
+    address: {sel: '.poi-item-details', attr: 'title'},
+    link: {sel: '.poi-item-name a', attr: 'href'},
   }
 };
 
 var linkScraper = {
-	iterator: 'ul.datasheet-more-infos li:last',
-	data: {
-		website: {sel: 'a:first', method: 'text'}
-	}
+  iterator: 'ul.datasheet-more-infos li:last',
+  data: {
+    website: {sel: 'a:first', method: 'text'}
+  }
 };
 
 var results = artoo.scrape(listScraper);
 var urlList = results.map(item => item.link)
 
 artoo.ajaxSpider(urlList, {
-	scrapeOne: linkScraper,
-	done: function (data) {
-		var newResults = results.map((item, i) => {
-			console.log("restaurant:", item);
-			console.log("website:", data[i]);
-			delete item.link;
-			item.website = data[i].website;
-			return item;
-		});
+  scrapeOne: linkScraper,
+  done: function (data) {
+    var newResults = results.map((item, i) => {
+      console.log("restaurant:", item);
+      console.log("website:", data[i]);
+      delete item.link;
+      item.website = data[i].website;
+      return item;
+    });
 
-		artoo.saveCsv(newResults);
-	}
+    artoo.saveCsv(newResults);     // can also use artoo.savePrettyJson();
+  }
 });
